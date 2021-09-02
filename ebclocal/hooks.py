bb9b0294d19e2e7f94b602021adf31f1daa95a8a
周 <zx1174717@163.com>
2021-09-02 12:59:50 +0000

    update ebclocal/hooks.py.

--- ebclocal/hooks.py
+++ ebclocal/hooks.py
@@ -16,7 +16,7 @@ app_license = "MIT"
 
 # include js, css files in header of desk.html
 # app_include_css = "/assets/ebclocal/css/ebclocal.css"
-app_include_js = "assets/js/ebclocal.min.js"
+app_include_js = "/assets/js/ebclocal.min.js"
 
 # include js, css files in header of web template
 # web_include_css = "/assets/ebclocal/css/ebclocal.css"
