05a55f3bea50bbb724c7c70ac2c70d181301829e
周 <zx1174717@163.com>
2021-09-02 13:03:48 +0000

    update ebclocal/public/js/hooks/page/permission_manager.js.

--- ebclocal/public/js/hooks/page/permission_manager.js
+++ ebclocal/public/js/hooks/page/permission_manager.js
@@ -1,7 +1,6 @@
 // 角色权限管理选择框改成可输入查找类型
-const MyPermissionEngine = frappe.PermissionEngine.extend({
-    setup_page: function() {
-		var me = this;
+frappe.PermissionEngine = class MyPermissionEngine extends frappe.PermissionEngine {
+	setup_page() {
 		this.doctype_select
 			= this.wrapper.page.add_auto_select(__("Document Type"), this.options.doctypes, 
 				function(e) {
@@ -12,14 +11,12 @@ const MyPermissionEngine = frappe.PermissionEngine.extend({
 		this.role_select
 			= this.wrapper.page.add_auto_select(__("Roles"), this.options.roles,
 				function() {
-					me.refresh();
+					this.refresh();
 				});
 
 		this.page.add_inner_button(__('Set User Permissions'), () => {
 			return frappe.set_route('List', 'User Permission');
 		});
 		this.set_from_route();
-	},
-})
-
-frappe.PermissionEngine = MyPermissionEngine;
+    }
+}
